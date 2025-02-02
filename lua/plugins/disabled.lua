443a73c3c20d3bb14eb802acee11475e132ce1f9
Enzo Lazzarini <enzolazzamor@gmail.com>
2025-02-02 18:49:10 -0300

    changed

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,4 @@
 return {
   { "stevearc/conform.nvim", enabled = false },
+  { "hrsh7th/nvim-cmp", enabled = false },
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,14 +5,5 @@ return {
     opts = {
       preset = "modern",
     },
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer Local Keymaps (which-key)",
-      },
-    },
   },
 }
