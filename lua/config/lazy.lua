ff19bdc93e579ef5b86e5a208b9934955856fe13
Enzo Lazzarini <enzolazzamor@gmail.com>
2025-02-19 20:31:18 -0300

    re-enabled conform

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -51,3 +51,22 @@ require("lazy").setup({
     },
   },
 })
+
+require("conform").setup({
+  formatters_by_ft = {
+    lua = { "stylua" },
+    -- Conform will run multiple formatters sequentially
+    python = { "isort", "black" },
+    -- You can customize some of the format options for the filetype (:help conform.format)
+    rust = { "rustfmt", lsp_format = "fallback" },
+    -- Conform will run the first available formatter
+    javascript = { "prettierd", "prettier", stop_after_first = true },
+
+    cpp = { "astyle" },
+  },
+})
+
+require("lint").linters_by_ft = {
+  markdown = { "vale" },
+  cpp = { "cppcheck", "cpplint" },
+}
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,1 @@
-return {
-  { "stevearc/conform.nvim", enabled = false },
-  { "hrsh7th/nvim-cmp", enabled = false },
-}
+return {}
