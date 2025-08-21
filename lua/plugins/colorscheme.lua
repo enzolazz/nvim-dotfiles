d3c8e926987690c9ae8a2c4cbb480d4a7d20058e
Enzo Lazzarini <enzolazzamor@gmail.com>
2025-08-21 18:06:00 -0300

    fix: bufferline was bugging with catppuccin so now the colorscheme is tokyonight: night.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,23 @@
 return {
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night" },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
-      style = "mocha",
+      colorscheme = "tokyonight",
     },
   },
+  -- {
+  --   "akinsho/bufferline.nvim",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     if (vim.g.colors_name or ""):find("catppuccin") then
+  --       opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+  --     end
+  --   end,
+  -- },
 }
