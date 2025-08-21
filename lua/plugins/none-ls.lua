5897ea522ad8c2764cb6e636b80867d13be13ee3
Enzo Lazzarini <enzolazzamor@gmail.com>
2025-08-21 15:58:47 -0300

    Prettier with tailwind I hope

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,9 +5,18 @@ return {
     local nls = require("null-ls")
 
     local formatting = nls.builtins.formatting
+    local diagnostics = nls.builtins.diagnostics
+
     opts.sources = opts.sources or {}
     vim.list_extend(opts.sources, {
       formatting.prettier,
+      formatting.duster,
+      diagnostics.djlint,
+    })
+
+    formatting.prettier.with({
+      command = "prettier",
+      extra_args = { "--plugin", "prettier-plugin-tailwindcss" },
     })
   end,
 }
